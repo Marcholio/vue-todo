b67cb79bf3ede5535421a37a90663de2eb8a84fc
Markus <markus.tyrkko@gmail.com>
2020-10-18 13:16:43 +0300

    Test build pipeline

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1>Todo-application</h1>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App!" />
   </div>
 </template>
 
